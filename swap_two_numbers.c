b691c010c89929b0f88ed6d71f0e7ff840b3b16d
Gayathri Ravi <gayathriravi218@gmail.com>
2025-10-10 06:38:07 +0530

    Update swap_two_numbers.c

--- swap_two_numbers.c
+++ swap_two_numbers.c
@@ -4,6 +4,7 @@ int main()
     int a, b, temp;
     printf("Enter two numbers: ");
     scanf("%d%d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
     temp = a;
     a = b;
     b = temp;
